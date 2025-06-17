d9e5ba946010c270b39fb055b814298b4ec8b441
ijaolahawwalolamilekan <ijaolahawwal@gmaol.com>
2025-06-17 19:59:48 +0100

    adde

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home"
 import ApplyNow from "./Pages/Applynow"
+import HeroSection from "./Pages/Hero";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Hero" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/Applynow" element={<ApplyNow/>} />
+        <Route path="/Hero" element={<HeroSection/>} />
       </Routes>
     </Router>
   );
